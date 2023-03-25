afac6675499a9f74570539d21317e195704f864d
boris lazarev <blazarev@BLAZAREV2-MAC.local>
2023-03-25 06:36:00 +0100

    fix: refactor webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,12 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPluging = require('webpack/lib/container/ModuleFederationPlugin');
 
-const ID_DEV = process.env.NODE_ENV === 'dev';
+const IS_DEV = process.env.NODE_ENV === 'dev';
+const ProductsRemote = IS_DEV ? 'http://localhost:8081': 'https://robzarel.github.io/mf-products/static';
+const CartRemote = IS_DEV ? 'http://localhost:8082': 'https://robzarel.github.io/mf-cart/static';
 
 module.exports = {
-  mode: ID_DEV ? 'development': 'production',
+  mode: IS_DEV ? 'development': 'production',
   output: {
     filename: '[name].[hash].js',
     path: path.resolve(__dirname, 'build/static'),
@@ -18,8 +20,8 @@ module.exports = {
     new ModuleFederationPluging({
       name: 'container',
       remotes: {
-        products: ID_DEV ? 'products@http://localhost:8081/remoteEntry.js' : 'products@https://robzarel.github.io/mf-products/static/remoteEntry.js',
-        cart: ID_DEV ? 'cart@http://localhost:8082/remoteEntry.js' : 'cart@https://robzarel.github.io/mf-cart/static/remoteEntry.js',
+        products: `products@${ProductsRemote}/remoteEntry.js`,
+        cart: `cart@${CartRemote}/remoteEntry.js`,
       },
     }),  
     new HtmlWebpackPlugin({
