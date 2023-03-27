4ba22561df7638d067ae0ff6b2e4af9037a6efd8
robzarel <robzarel@yandex.ru>
2023-03-27 11:20:51 +0200

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
-const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPluging = require('webpack/lib/container/ModuleFederationPlugin');
 
 const IS_DEV = process.env.MODE === 'dev';
-const ProductsRemote = IS_DEV ? 'http://localhost:8081': 'https://robzarel.github.io/mf-products/static';
-const CartRemote = IS_DEV ? 'http://localhost:8082': 'https://robzarel.github.io/mf-cart/static';
+const ProductsRemote = IS_DEV ? 'http://localhost:8081': 'https://robzarel.github.io/mf-products';
+const CartRemote = IS_DEV ? 'http://localhost:8082': 'https://robzarel.github.io/mf-cart';
 
 const config = {
   mode: IS_DEV ? 'development': 'production',
+  output: {
+    filename: '[name].[hash].js',
+  },
   devServer: {
     port: 8080,
   },
@@ -20,18 +22,9 @@ const config = {
       },
     }),  
     new HtmlWebpackPlugin({
-      filename: IS_DEV ? './index.html' : '../index.html',
       template: './public/index.html',
     })
   ]
 };
-
-if (!IS_DEV) {
-  config.output = {
-    filename: '[name].[hash].js',
-    path: path.resolve(__dirname, 'build/static'),
-    publicPath: 'static/'
-  };
-}
 
 module.exports = config;
